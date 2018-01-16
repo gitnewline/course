090b31fe7b46cd3ce33651cfb3f451458f00c978
newline <newline@newline-pc3>
2018-01-16 20:28:52 +0600

    sss

--- src/Task_03.java
+++ src/Task_03.java
@@ -2,11 +2,7 @@
 public class Task_03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       int n = 179 ,jauap;
-        jauap = n % 10;
-        
-        System.out.println( n + "sanyn en sangy cifrasy - " + jauap  );  
+	 
 	}
 
 }
