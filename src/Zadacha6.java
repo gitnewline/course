5b3ac2af63dd2bd7ed8e670f49f949b4bcfe0ca0
newline <newline@newline-pc3>
2018-01-22 16:42:14 +0600

    Esep 6

---
+++ src/Zadacha6.java
@@ -0,0 +1,22 @@
+
+public class Zadacha6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a= 4, b = 4, c = 4;
+		
+		double s, p;
+		p = (a + b+ c)/2;
+		s = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+		
+		System.out.println(p); 
+		System.out.println(s);
+		
+	}
+
+	
+		 
+	}
+
+
